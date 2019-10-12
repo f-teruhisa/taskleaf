48eb2bc691e1b8e93cef02eb10c475ca820fa1da
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-10-12 16:42:40 +0900

    fix spec test in login path

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,20 +2,30 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
-    before do
-      user_a  = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@exmple.com')
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-      context 'ユーザーAがログインしているとき' do
-        before do
-          visit login_path
-          fill_in 'メールアドレス', with: 'a@example.com'
-          fill_in 'パスワード', with: 'password'
-          click_button 'ログインする'
-        end
+    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@exmple.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@exmple.com') }
 
-        it 'ユーザーAが作成したタスクが表示される' do
-          expect(page).to have_content '最初のタスク'
-        end
+    before do
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
+    end
+
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+
+    context 'ユーザーBがログインしているとき' do
+      let(:login_user) { user_b }
+
+      it 'ユーザーAが作成したタスクが表示されない' do
+        expect(page).to have_no_content '最初のタスク'
       end
     end
   end
